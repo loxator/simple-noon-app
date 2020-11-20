1a498d45aba34f61c20bfc41af9f7eea05b0ade6
Amin Limbada <amin.limbada@consensys.net>
2020-11-21 01:08:01 +0400

    Create /favourites page without functionality

---
+++ pages/favorites.js
@@ -0,0 +1,22 @@
+import Layout from "../components/Layout/Layout";
+import Posts from "../components/Posts/Posts";
+
+const Favorites = () => (
+  <Layout>
+    <div className="index-container">
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 1].map(() => (
+        <Posts />
+      ))}
+    </div>
+    <style jsx>{`
+      @media only screen and (min-device-width: 800px) {
+        .index-container {
+          display: flex;
+          flex-wrap: wrap;
+        }
+      }
+    `}</style>
+  </Layout>
+);
+
+export default Favorites;
